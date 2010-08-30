dcd01ecd879872ebe7d129c000996cf18a12a857
sevi <sevi@sevis-MacBook-2.local>
2010-08-30 22:40:37 +0200

    Changed names and added pysieve module

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from distutils.core import setup, Extension
 
-module1 = Extension('_pysieve',
+_pysieve = Extension('_pysieve',
                     include_dirs = ['/opt/local/include/msieve'],
                     libraries = ['msieve', 'gmp'],
                     library_dirs = ['/opt/local/lib/'],
                     sources = ['pysieve.c'])
 
-setup (name = '_pysieve',
+setup (name = 'pysieve',
        version = '0.0',
        description = 'prime factors',
-       ext_modules = [module1])
+       py_modules = ['pysieve'],
+       ext_modules = [_pysieve])
