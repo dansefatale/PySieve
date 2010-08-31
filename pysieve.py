75ed162a0427bb9cfa80bd25dec73d5086d0118e
sevib <schreib.mal.sevi@gmail.com>
2010-08-31 22:01:09 +0200

    Added the dfactor function and fixed the exponent count

--- pysieve.py
+++ pysieve.py
@@ -4,21 +4,25 @@ def factor(n):
     """ Returns a list of the prime factors of n """
     return map(int, _pysieve.pysieve(str(n)))
 
+def dfactor(n):
+    """ Returns a list of distinct prime factors of n """
+    return list(set(factor(n)))
+
 def facExponents(n):
     """ Return a list of tuples containing the prime factors of n and their exponents """
 
     # determine the exponent of fac in n
-    def pfind(fac,n):
+    def expfind(fac, n):
         if (n/fac%fac != 0):
             return 1
         else:
-            return 1 + pfind(fac, n/fac)
+            return 1 + expfind(fac, n/fac)
 
     def result(list):
         return zip(list,
                    (lambda(x):
-                        map(pfind, x, [n]*len(x)))(list))
+                        map(expfind, x, [n]*len(x)))(list))
 
-    return result(factor(n))
+    return result(dfactor(n))
 
 
